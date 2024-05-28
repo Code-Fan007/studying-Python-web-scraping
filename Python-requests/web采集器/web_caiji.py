1e99c45c78cb1b7e94ac120dc9721cf74ae80a97
Code_Mr_Fan <1738874436@qq.com>
2024-05-28 15:07:31 +0800

    测试

---
+++ Python-requests/web采集器/web_caiji.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3.11
+# -*- coding: utf-8 -*-
+import requests
+#if这行的意思是：在代码运行的时候，会有一个内置变量，__name__，
+#将其命名为__main__，一般用来自己写模块的时候调试用的，这样导入这个模块，就不会执行下面的语句
+if __name__=="_main__":
+    url = "https://www.baidu.com"
+
+    
